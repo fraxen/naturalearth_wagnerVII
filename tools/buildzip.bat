daab9ad7fbdbb458528e3b5586011e454c712ca3
Hugo Ahlenius <hugo.ahlenius@nordpil.com>
2014-06-16 13:35:45 +0200

    more exclusions

--- tools/buildzip.bat
+++ tools/buildzip.bat
@@ -1,6 +1,6 @@
 pushd %~dp0
 pushd ..
 del naturalearth_wagnervii.zip
-zip -r naturalearth_wagnervii.zip . -x tools/* -x zia* -x *.lock
+zip -r naturalearth_wagnervii.zip . -x tools/* -x zia* -x *.lock -x .gitignore -x .git
 popd
 popd
